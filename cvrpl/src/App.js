aee60b396d0f8515662239a89132511c02ef9bb3
Ryan Gilmour <32744212+gilmoid@users.noreply.github.com>
2021-11-17 11:15:18 -0500

    updating

--- cvrpl/src/App.js
+++ cvrpl/src/App.js
@@ -1,10 +1,12 @@
-import picture from './Alaska.jpg';
+import React from 'react'
 import './App.css';
-import Avatar from './Avatar.js';
 import Navbar from './Navbar.js';
+import Profile from './Profile.js';
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth, signInWithRedirect, GoogleAuthProvider } from "firebase/auth";
+import { getAuth, signInWithRedirect, GoogleAuthProvider, getRedirectResult, onAuthStateChanged} from "firebase/auth";
+import { getFirestore } from "firebase/firestore"
+import { getStorage, ref , getDownloadURL } from "firebase/storage"
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -22,16 +24,33 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 const auth = getAuth();
+let user;
 const googleAuth = GoogleAuthProvider;
+const getRedirect = getRedirectResult;
 const provider = new GoogleAuthProvider();
 const signIn = signInWithRedirect;
+const storage = getStorage();
+// const pathReference = ref(storage, 'You Found It.mp3')
+const pathReference = ref(storage, 'AnryGuiltar/arabesque.mp3')
+console.log(pathReference._location.path_);
+let nothing = 0;
 
 function App() {
+  const [propic, setPropic] = React.useState(auth.currentUser ? auth.currentUser.providerData[0].photoURL : null);
+  const [songSource, setSongSource] = React.useState(null);
+  onAuthStateChanged(auth, () => setPropic(auth.currentUser ? auth.currentUser.providerData[0].photoURL : null) );
+  getDownloadURL(pathReference).then((url) => {
+    setSongSource(url);
+  })
+  .catch((error) => {
+    console.log(error)
+  })
   return (
     <div className="App">
-      <Navbar auth={auth} provider={provider} signIn={signIn} googleAuth={googleAuth}/>
-      {/* <Navbar /> */}
+      <Navbar auth={auth} provider={provider} signIn={signIn} googleAuth={googleAuth} getRedirect={getRedirect} propic={propic} setPropic={setPropic}/>
+      <Profile auth={auth} propic={propic} songSource={songSource}/>
     </div>
   );
 }
--- cvrpl/src/Navbar.js
+++ cvrpl/src/Navbar.js
@@ -6,7 +6,7 @@ function Navbar(props) {
         <>
             <div className="Navbar">
                 <h1 className="Header">Coverpool</h1>
-                <Avatar signIn={props.signIn} auth={props.auth} provider={props.provider}/>
+                <Avatar signIn={props.signIn} auth={props.auth} provider={props.provider} googleAuth={props.googleAuth} getRedirect={props.getRedirect} propic={props.propic} setPropic={props.setPropic}/>
             </div>
             <hr></hr>
         </>
